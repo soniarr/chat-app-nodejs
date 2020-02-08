83ec30cfa0fc5eb041b5eb66528bcd040b87a9c3
soniarr <18328580+soniarr@users.noreply.github.com>
2020-02-08 19:48:12 +0100

    Add EndPoint

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,13 +5,24 @@ import io from "socket.io-client";
 
 import './Chat.css';
 
+let socket;
+
 const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
-        // 1) Retrieve the data
+        // Retrieve the data
         const {name, room} = queryString.parse(location.search);
         
-        console.log(name, room);
+        // Endpoint
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
     });
 
     return (
