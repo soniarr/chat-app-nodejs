de44ed63f9cb52a6e9df494b333c28522b2941a9
soniarr <18328580+soniarr@users.noreply.github.com>
2020-02-08 17:20:26 +0100

    Step 2: Basic react.js website

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// All the code will be here:
+import App from './App'
+
+// Then will put the code into /public/index.html --> div#root
+ReactDOM.render(<App />, document.querySelector('#root'));
