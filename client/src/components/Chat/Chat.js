0a4edc96d84938555fb55d5f1c260bcb4665793f
soniarr <18328580+soniarr@users.noreply.github.com>
2020-02-08 19:39:56 +0100

    Retrieving data: Get URL params

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,19 @@
-import React from 'react';
+// useEffect is a hook https://es.reactjs.org/docs/hooks-effect.html
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from "socket.io-client";
 
 import './Chat.css';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+
+    useEffect(() => {
+        // 1) Retrieve the data
+        const data = queryString.parse(location.search);
+        console.log(location.search); // Return URL params (?name=XXX&room=YYY)
+        console.log(data); // Object with the URL params ({"name": "XXX", "room": "YYY"})
+    });
+
     return (
         <h1>Chat</h1>
     )
