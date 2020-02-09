c585c403ee629d5af4f1061cf611875a511eaeb8
soniarr <18328580+soniarr@users.noreply.github.com>
2020-02-09 14:30:52 +0100

    Disconnect user when refresh page

--- server/index.js
+++ server/index.js
@@ -38,7 +38,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User has left!!');
+        const user = removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
+        }
     });
 });
 
