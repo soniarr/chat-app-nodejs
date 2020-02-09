6e5b388baee0a22c53d763e56c9f154629446ef0
soniarr <18328580+soniarr@users.noreply.github.com>
2020-02-09 13:50:46 +0100

    Import and using users managing functions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+// Import functions from /server/users.js
+const { adduser, removeUser, getUser, getUserInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000
 
 const router = require('./router');
@@ -11,10 +14,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!');
 
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = adduser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.join(user.room);
     });
 
     socket.on('disconnect', () => {
